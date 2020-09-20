249cc0a6b6531e83a5da84f89bea0d059d5c2243
Serge BESSON <serge@famille-besson.com>
2020-09-20 10:20:26 +0200

    utilisation de `@sbesson/json-db` au lieu de `sb-collection-file`

--- server/stores/users.store.js
+++ server/stores/users.store.js
@@ -3,15 +3,14 @@
 const _ = require("lodash");
 const path = require("path");
 const { safeCompare } = require("express-basic-auth");
-
-const { CollectionFile } = require("sb-collection-file");
+const { JsonDb } = require("@sbesson/json-db");
 
 const { User } = require("./user");
 
 class UsersStore {
 	constructor({ configLoader }) {
 		const directory = configLoader.getValue("storage.databaseDirectory");
-		this.structureCollectionFile = {
+		this.structureJsonDb = {
 			idName: "name",
 			jsonSchema: {
 				type: "object",
@@ -27,7 +26,7 @@ class UsersStore {
 			searchIndex: [ "name", "description" ],
 		};
 
-		this.collectionFile = new CollectionFile(path.join(directory, "users"));
+		this.jsonDb = new JsonDb(path.join(directory, "users"));
 
 		this._loginUser = null;
 		this._usersExpressBasicAuth = null;
@@ -35,13 +34,13 @@ class UsersStore {
 
 	async initialize() {
 		try {
-			await this.collectionFile.loadCollection();
+			await this.jsonDb.loadCollection();
 		} catch (error) {
 			if (error.code !== "ENOENT") {
 				throw error;
 			}
-			await this.collectionFile.create(this.structureCollectionFile);
-			await this.collectionFile.insert({
+			await this.jsonDb.create(this.structureJsonDb);
+			await this.jsonDb.insert({
 				name: "serge",
 				password: "achanger",
 				active: true,
@@ -51,7 +50,7 @@ class UsersStore {
 	}
 
 	set loginUser(name) {
-		const user = this.collectionFile.collection.getById(name);
+		const user = this.jsonDb.collection.getById(name);
 		if (!user) {
 			throw new Error(`loginUser - utilisateur ${ name } inconnu`);
 		}
@@ -63,7 +62,7 @@ class UsersStore {
 
 	checkAuthentification({ name: nameInput, password: passwordInput }) {
 		return _.some(
-			this.collectionFile.collection.find({ active: true }),
+			this.jsonDb.collection.find({ active: true }),
 			({ name, password }) =>
 				// eslint-disable-next-line no-bitwise
 				safeCompare(nameInput, name) & safeCompare(passwordInput, password),
@@ -71,12 +70,12 @@ class UsersStore {
 	}
 
 	search({ query, search }) {
-		return this.collectionFile.collection.search({ query, search })
+		return this.jsonDb.collection.search({ query, search })
 			.map((user) => new User(user).jsonPrivate);
 	}
 
 	getUserByName({ name }) {
-		const user = this.collectionFile.collection.getById(name);
+		const user = this.jsonDb.collection.getById(name);
 		if (!user) {
 			return null;
 		}
@@ -84,22 +83,22 @@ class UsersStore {
 	}
 
 	async insert(userInfos) {
-		const { document: user } = await this.collectionFile.insert(new User(userInfos).json);
+		const { document: user } = await this.jsonDb.insert(new User(userInfos).json);
 		return new User(user);
 	}
 
 	async update(userUpdateInfos) {
-		const userInfos = this.collectionFile.collection.getById(userUpdateInfos.name);
+		const userInfos = this.jsonDb.collection.getById(userUpdateInfos.name);
 		if (!userInfos) {
 			throw new Error("userNotfound");
 		}
 		const user = new User({ ...userInfos, ..._.omitBy(userUpdateInfos, _.isUndefined) });
-		const { document: userUpdate } = await this.collectionFile.update(user.json);
+		const { document: userUpdate } = await this.jsonDb.update(user.json);
 		return new User(userUpdate);
 	}
 
 	async delete({ name }) {
-		const userInfos = this.collectionFile.collection.getById(name);
+		const userInfos = this.jsonDb.collection.getById(name);
 		if (!userInfos) {
 			throw new Error("userNotfound");
 		}
@@ -107,7 +106,7 @@ class UsersStore {
 		if (user.isAdmin) {
 			throw new Error("unauthorized");
 		}
-		await this.collectionFile.delete(name);
+		await this.jsonDb.delete(name);
 	}
 }
 
